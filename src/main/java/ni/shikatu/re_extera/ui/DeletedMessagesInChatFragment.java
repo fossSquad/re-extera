6ee677ea2c4137e07d3116de40515cf999dc52a3
aartzz <migor1103@gmail.com>
2026-09-01 14:52:07 +0300

    fix(ui): adapt ChatMessageCellDelegate button callbacks for 12.10.1

--- src/main/java/ni/shikatu/re_extera/ui/DeletedMessagesInChatFragment.java
+++ src/main/java/ni/shikatu/re_extera/ui/DeletedMessagesInChatFragment.java
@@ -546,7 +546,7 @@ public class DeletedMessagesInChatFragment extends BaseFragment implements ChatM
         return false;
     }
 
-    public void didPressExtendedMediaPreview(ChatMessageCell cell, TLRPC.KeyboardButton button) {
+    public void didPressExtendedMediaPreview(ChatMessageCell cell, org.telegram.tgnet.tl.TL_keyboard.KeyboardInlineButton button) {
     }
 
     public void didPressUserStatus(ChatMessageCell cell, TLRPC.User user, TLRPC.Document document, String giftSlug) {
@@ -683,10 +683,10 @@ public class DeletedMessagesInChatFragment extends BaseFragment implements ChatM
     public void didPressTime(ChatMessageCell cell) {
     }
 
-    public void didPressBotButton(ChatMessageCell cell, TLRPC.KeyboardButton button) {
+    public void didPressBotButton(ChatMessageCell cell, org.telegram.tgnet.tl.TL_keyboard.KeyboardButtonProto button) {
     }
 
-    public void didLongPressBotButton(ChatMessageCell cell, TLRPC.KeyboardButton button) {
+    public void didLongPressBotButton(ChatMessageCell cell, org.telegram.tgnet.tl.TL_keyboard.KeyboardButtonProto button) {
     }
 
     public void didPressCustomBotButton(ChatMessageCell cell, BotInlineKeyboard.ButtonCustom button) {
